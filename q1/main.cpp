4fbfea150a2f5468aaf59d5e5b295dbbe661fb18
Mike Chaberski <mchaberski@gmail.com>
2020-03-22 21:58:42 -0400

    q1 implemented

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,44 @@
-// <author>
+// mac937@nyu.edu
 // hw11 question 1
 
 #include <iostream>
 
 using namespace std;
 
+const char STAR = '*';
+
+void printStarLine(int length) {
+    if (length > 0) {
+        cout << STAR;
+        printStarLine(length - 1);
+    } else {
+        cout << endl;
+    }
+}
+
+void printTriangle(int n) {
+    if (n > 0) {
+        printTriangle(n - 1);
+        printStarLine(n);
+    }
+}
+
+void printOppositeTriangles(int n) {
+    if (n > 0) {
+        printStarLine(n);
+        printOppositeTriangles(n - 1);
+        printStarLine(n);
+    }
+}
+
 int main() {
-    cout << "q1 executed" << endl;
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Part A" << endl;
+    printTriangle(n);
+    cout << endl;
+    cout << "Part B" << endl;
+    printOppositeTriangles(n);
     return 0;
 }
--- q1/question.md
+++ q1/question.md
@@ -1,3 +1,36 @@
 # Question 1
 
-Write a program...
+Give a recursive implement to the following functions.
+
+# Part A
+
+    void printTriangle(int n)
+
+This function is given a positive integer n, and prints a textual 
+image of a right triangle (aligned to the left) made of n lines 
+with asterisks.
+
+For example, printTriangle(4), should print:
+
+    *
+    **
+    ***
+    ****
+
+## Part B
+
+    void printOppositeTriangles(int n)
+
+This function is given a positive integer n, and prints a textual image of a two opposite right
+triangles (aligned to the left) with asterisks, each containing n lines.
+For example, printOppositeTriangles(4), should print:
+
+    ****
+    ***
+    **
+    *
+    *
+    **
+    ***
+    ****
+    
